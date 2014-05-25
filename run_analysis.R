d2d1439e7939cd5cc8bd7467eaebfcccbb6aea19
Jose Antonio Mancilla <jt.mancilla@gmail.com>
2014-05-25 00:16:43 -0500

    second commit

--- run_analysis.R
+++ run_analysis.R
@@ -1,37 +1,35 @@
 library(reshape2)
 #Read Data
-train_set <- read.table("D:\\RStudio\\UCI HAR Dataset\\train\\X_train.txt")
-train_label <- read.table("D:\\RStudio\\UCI HAR Dataset\\train\\y_train.txt", col.names="label")
-test_set <- read.table("D:\\RStudio\\UCI HAR Dataset\\test\\X_test.txt")
-test_label <- read.table("D:\\RStudio\\UCI HAR Dataset\\test\\y_test.txt", col.name="label")
+TrainSet <- read.table("D:\\RStudio\\UCI HAR Dataset\\train\\X_train.txt")
+TrainLabel <- read.table("D:\\RStudio\\UCI HAR Dataset\\train\\y_train.txt", col.names="label")
+TestSet <- read.table("D:\\RStudio\\UCI HAR Dataset\\test\\X_test.txt")
+TestLabel <- read.table("D:\\RStudio\\UCI HAR Dataset\\test\\y_test.txt", col.name="label")
 
 #Merge Training and test data
-train_test_set2 <- rbind(train_set,test_set)
+TrainTestSet2 <- rbind(TrainSet,TestSet)
 
 #Extract only mean and standard deviation from the full set
-features <- read.table("D:\\RStudio\\UCI HAR Dataset\\features.txt")
-num_colunmmean <- grep("mean()",features$V2)
-name_colunmmean <- grep("mean()",features$V2, value=TRUE)
-num_colunmstd <- grep("std()",features$V2)
-name_colunmstd <- grep("std()",features$V2, value =TRUE)
-data_mean_std <- train_test_set2[,c(num_colunmmean,num_colunmstd)]
-names(data_mean_std) <- c(name_colunmmean,name_colunmstd)
+Features <- read.table("D:\\RStudio\\UCI HAR Dataset\\features.txt")
+NumColunmMean <- grep("mean()",Features$V2)
+NameColunmMean <- grep("mean()",Features$V2, value=TRUE)
+NumColunmStd <- grep("std()",Features$V2)
+NameColunmStd <- grep("std()",Features$V2, value =TRUE)
+DataMeanStd <- TrainTestSet2[,c(NumColunmMean,NumColunmStd)]
+names(DataMeanStd) <- c(NameColunmMean,NameColunmStd)
 
 #Combination of subject and activity to create tidy data
-train_test_label2 <- rbind(train_label,test_label)
-Data_label3 <- cbind(data_mean_std,train_test_label2)
-act_label <- read.table("D:\\RStudio\\UCI HAR Dataset\\activity_labels.txt")
-subject_tr <- read.table("D:\\RStudio\\UCI HAR Dataset\\train\\subject_train.txt")
-subject_te <- read.table("D:\\RStudio\\UCI HAR Dataset\\test\\subject_test.txt")
-subject <- rbind(subject_tr,subject_te)
+TrainTestLabel2 <- rbind(TrainLabel,TestLabel)
+DataLabel3 <- cbind(DataMeanStd,TrainTestLabel2)
+ActLabel <- read.table("D:\\RStudio\\UCI HAR Dataset\\activity_labels.txt")
+SubjectTr <- read.table("D:\\RStudio\\UCI HAR Dataset\\train\\subject_train.txt")
+SubjectTe <- read.table("D:\\RStudio\\UCI HAR Dataset\\test\\subject_test.txt")
+Subject <- rbind(SubjectTr,SubjectTe)
 names(subject) <- "subject"
-Data <- cbind(Data_label3,subject)
-names(act_label) <- c("label","action")
-mergeData = merge(Data, act_label, by.x="label",by.y="label",all=TRUE)
-mergeData$label<- NULL
-meltData <-melt(mergeData, id=c("subject", "action"))
-tidy_Data <- dcast(meltData, ...~ variable, mean)
-
+Data <- cbind(DataLabel3,Subject)
+names(ActLabel) <- c("label","action")
+MergeData = merge(Data, ActLabel, by.x="label",by.y="label",all=TRUE)
+MergeData$label<- NULL
+MeltData <-melt(MergeData, id=c("subject", "action"))
+TidyData <- dcast(MeltData, ...~ variable, mean)
 #Saved the tidy data
-write.table(tidy_Data, "Samsung_Dataset.txt", sep="\t")
-
+write.table(TidyData, "Samsung_Dataset.txt", sep="\t")
